12ced915f04a4f7b6a94ffe04220a46d869bec01
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-12-26 19:43:04 +0600

    Lesson commit

---
+++ TCPServer/TCPServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 3231);
+
+            listener.Start();
+            Console.WriteLine("Сервер запущен");
+
+            while (true)
+            {
+                using (var client = listener.AcceptTcpClient())
+                {
+                    Console.WriteLine("Входящее соединение.............");
+
+                    using (var stream = client.GetStream())
+                    {
+                        var resultStringBuilder = new StringBuilder();
+                        while (stream.DataAvailable)
+                        {
+                            var buffer = new byte[24];
+                            stream.Read(buffer, 0, buffer.Length);
+                            resultStringBuilder.Append(Encoding.UTF8.GetString(buffer));
+                        }
+
+                        Console.WriteLine($"Данные от клиента - {resultStringBuilder.ToString()}");
+
+                        var answerData = Encoding.UTF8.GetBytes("Запрос получен...");
+                        stream.Write(answerData, 0, answerData.Length);
+                    }
+                }
+                Console.WriteLine("Соединение закрыто");
+            }
+        }
+    }
+}
+
